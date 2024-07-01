9c69508c79b9929ae4cfec2fcad76e9972ee2ce8
John Paul Dela Cruz <johnd@scorptec.com.au>
2024-07-01 13:24:48 +0800

    Update Title for home page

--- resources/views/Home/HomeBody.blade.php
+++ resources/views/Home/HomeBody.blade.php
@@ -1,5 +1,5 @@
 <div>
-    <figure class="figure">
+    <figure class="figure px-5">
         <img src="https://img.freepik.com/premium-vector/japan-travel-map_81894-302.jpg?w=826" class="figure-img img-fluid rounded" alt="...">
         <figcaption class="figure-caption center-block">
             Check tourist spots in Japan and see how is the climate in those places.
