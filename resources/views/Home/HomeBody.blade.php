4a775d2c87ad5c478ca5d9930ef9b779f9779c08
John Paul Dela Cruz <johnd@scorptec.com.au>
2024-07-03 17:08:40 +0800

    Replace image in the home page and centered

--- resources/views/Home/HomeBody.blade.php
+++ resources/views/Home/HomeBody.blade.php
@@ -1,6 +1,6 @@
-<div>
+<div class="d-block text-center">
     <figure class="figure px-5">
-        <img src="https://img.freepik.com/premium-vector/japan-travel-map_81894-302.jpg?w=826" class="figure-img img-fluid rounded" alt="...">
+        <img src="https://stage.caspio.com/demo/ebandong/jp/japan.png" class="figure-img img-fluid rounded" alt="...">
         <figcaption class="figure-caption center-block">
             Check tourist spots in Japan and see how is the climate in those places.
         </figcaption>
